7f7f78e6fae904bd10268b8acd483678eeb4ef07
Ramadan Alaa <ramadan.alaa.pro@gmail.com>
2026-07-05 22:12:27 +0300

    feat: create base Layout component with gradient background

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="bg-[var(--BG_gradient)]">
+    <div className="bg-(--BG_gradient)">
       <Outlet />
     </div>
   );
